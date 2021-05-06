f7bcee9b65d016b8ddc3c6fafd6c1bef7b50d960
Tony Zhang <tony.j.h.zhang@gmail.com>
2021-05-06 18:08:36 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function updateData(value){
+    document.getElementById("cheese-amount").innerHTML = "Cups of cheese: " + value*2
+    document.getElementById("flour-amount").innerHTML = "Amount of flour: " + value*5
+
+}
